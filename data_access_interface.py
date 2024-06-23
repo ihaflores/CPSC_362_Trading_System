bfe745dbaaee65834e627f660c646a1295e5362d
Ian Flores <ihflores@csu.fullerton.edu>
2024-06-22 20:35:42 -0700

    Changed variable name to database

--- data_access_interface.py
+++ data_access_interface.py
@@ -1,13 +1,13 @@
 import database as db
 
 class DataAccessInterface(db.DataBase):
-    data = None
+    database = None
 
     def __init__(self, database):
-        self.data = database
+        self.database = database
 
     def download_data(self):
-        self.data.download_data()
+        self.database.download_data()
 
     def load_data(self):
-        return self.data.load_data()
+        return self.database.load_data()
